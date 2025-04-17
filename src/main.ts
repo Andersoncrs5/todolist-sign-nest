7f9cf7b47791f7a644cc4f90e0e5db4705a4ef7d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-04-16 21:45:23 -0300

    Fix anything

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
   await app.register(fastifyCors, {
     origin: 'http://localhost:5173',
     credentials: true,
-  });
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'] 
+  });  
 
   app.useGlobalPipes(
     new ValidationPipe({
